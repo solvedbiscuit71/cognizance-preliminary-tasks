69b56f76f9b5d80f47ddfe65e01dc25224250dcb
Praveen Perumal <ppraveen98841@gmail.com>
2022-12-31 16:10:37 +0530

    Bugfix and Refactor
    Resolved issue with updating the nextId, and refactor the code and
    reduced # of global variables

--- task2/todo/assets/script.js
+++ task2/todo/assets/script.js
@@ -1,87 +1,5 @@
-/* global variables */
-const ul = document.getElementById('todos')
-const form = document.getElementById('form')
-const submitBtn = document.getElementById('btn__add')
-const clearBtn = document.getElementById('btn__clear')
+/* Utils Functions */
 
-const listMessage = document.getElementById('list__message')
-const tracker = document.getElementById('tracker')
-
-let nextId = 0
-let todoList = []
-
-/* reactivity */
-const setTodoList = newList => {
-    // constructing a new DOM
-    ul.innerHTML = ''
-    generate(newList).forEach(li => {
-        ul.appendChild(li)
-    });
-
-    // toggle `list is empty` message
-    if (todoList.length != newList.length && (todoList.length == 0 || newList.length == 0))  {
-        listMessage.classList.toggle('hide')
-        ul.classList.toggle('hide')
-    }
-
-    // count no of active todos
-    tracker.innerText = newList.reduce((count, todo) => todo.isDone ? count - 1 : count, newList.length)
-
-    todoList = newList
-
-    localStorage.setItem('todoList', JSON.stringify(todoList))
-    localStorage.setItem('nextId', JSON.stringify(nextId))
-}
-
-/* handlers */
-const handleSubmit = _ => {
-
-    const head = form.children['head'].value
-    if (head === '')
-        return
-
-    const body = form.children['body'].value
-    const time = form.children[2].children['time'].value
-
-    setTodoList([...todoList, {
-        id: nextId++,
-        isDone: false,
-
-        head: head.length < 42 ? head : head.slice(0,42) + ' ...',
-        body: body || 'No Description',
-        time: time === '' ? '-' : format(time),
-    }])
-    form.reset()
-}
-
-const handleDelete = e => {
-    const id = e.composedPath()[0].dataset['id']
-    setTodoList(todoList.filter(todo => todo.id !== +id))
-}
-
-const handleDone = e => {
-    const id = e.composedPath()[0].dataset['id']
-    setTodoList(todoList.map(todo => {
-        if (todo.id == id)
-            todo.isDone = !todo.isDone
-        return todo
-    }))
-}
-
-const handleClear = _ => {
-    setTodoList(todoList.filter(todo => !todo.isDone))
-}
-
-/* attach event listeners */
-submitBtn.addEventListener('click', handleSubmit)
-clearBtn.addEventListener('click', handleClear)
-form.addEventListener('submit', e => e.preventDefault())
-
-/* load previous session todolist form localStorage */
-setTodoList(loadFromStorage('todoList', '[]'))
-nextId = loadFromStorage('nextId', '0')
-
-/* utility functions */
 function format(T) {
     const [date,time] = T.split('T')
     const [year, month, day] = date.split('-')
@@ -93,6 +11,20 @@ function format(T) {
 }
 
 function generate(L) {
+    const handleDelete = e => {
+        const id = e.composedPath()[0].dataset['id']
+        setTodoList(todoList.filter(todo => todo.id !== +id))
+    }
+
+    const handleDone = e => {
+        const id = e.composedPath()[0].dataset['id']
+        setTodoList(todoList.map(todo => {
+            if (todo.id == id)
+                todo.isDone = !todo.isDone
+            return todo
+        }))
+    }
+
     return L.map(todo => {
         const li = document.createElement('li')
         todo.isDone && li.classList.add('done')
@@ -111,3 +43,67 @@ function loadFromStorage(key, value) {
     
     return JSON.parse(localStorage.getItem(key))
 }
+
+/* State Variables */
+
+let todoList = []
+const setTodoList = newList => {
+    const ul = document.getElementById('todos')
+    const listMessage = document.getElementById('list__message')
+    const tracker = document.getElementById('tracker')
+
+    ul.innerHTML = ''
+    generate(newList).forEach(li => {
+        ul.appendChild(li)
+    });
+
+    if (todoList.length != newList.length && (todoList.length == 0 || newList.length == 0))  {
+        listMessage.classList.toggle('hide')
+        ul.classList.toggle('hide')
+    }
+    tracker.innerText = newList.reduce((count, todo) => todo.isDone ? count - 1 : count, newList.length)
+
+    todoList = newList
+    localStorage.setItem('todoList', JSON.stringify(todoList))
+}
+
+let nextId = 0
+const setNextId = newId => {
+    nextId = newId
+    localStorage.setItem('nextId', JSON.stringify(nextId))
+}
+
+setTodoList(loadFromStorage('todoList', '[]'))
+setNextId(loadFromStorage('nextId', '0'))
+
+
+/* Event Listener */
+
+document.getElementById('btn__add').addEventListener('click', _ => {
+    const form = document.getElementById('form')
+
+    const head = form.children['head'].value
+    if (head === '')
+        return
+
+    const body = form.children['body'].value
+    const time = form.children[2].children['time'].value
+
+    setTodoList([...todoList, {
+        id: nextId,
+        isDone: false,
+
+        head: head.length < 42 ? head : head.slice(0,42) + ' ...',
+        body: body || 'No Description',
+        time: time === '' ? '-' : format(time),
+    }])
+    setNextId(nextId + 1)
+
+    form.reset()
+})
+
+document.getElementById('btn__clear').addEventListener('click', _ => {
+    setTodoList(todoList.filter(todo => !todo.isDone))
+})
+
+document.getElementById('form').addEventListener('submit', e => e.preventDefault())
